3539f50e515a44c5b3c6a5cca5512bf0909bf3c8
HAD <had13625428701@aliyun.com>
2020-03-23 19:27:40 -0700

    v1

---
+++ src/main/java/com/had/pqst/CrawlerMain.java
@@ -0,0 +1,31 @@
+package com.had.pqst;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class CrawlerMain {
+    public static final Log log = LogFactory.getLog(CrawlerMain.class);
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://sina.cn");
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+        try {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            System.out.println(EntityUtils.toString(entity1));
+        } finally {
+            response1.close();
+        }
+
+
+    }
+}
---
+++ src/test/java/com/had/pqst/CrawlerTest.java
@@ -0,0 +1,8 @@
+package com.had.pqst;
+
+import org.junit.jupiter.api.Test;
+
+public class CrawlerTest {
+    @Test
+    public void test() {}
+}
