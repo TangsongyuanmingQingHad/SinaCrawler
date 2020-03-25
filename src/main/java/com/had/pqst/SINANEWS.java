7257a09cd6bcbed3e3956e66a5c7fb191a570d82
HAD <had13625428701@aliyun.com>
2020-03-25 16:22:29 -0700

    new class SINANEWS

---
+++ src/main/java/com/had/pqst/SINANEWS.java
@@ -0,0 +1,53 @@
+package com.had.pqst;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+public class SINANEWS {
+    private BigInteger id;
+    private String title;
+    private String content;
+    private Date createAt;
+    private Date modifyAt;
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getModifyAt() {
+        return modifyAt;
+    }
+
+    public void setModifyAt(Date modifyAt) {
+        this.modifyAt = modifyAt;
+    }
+
+}
